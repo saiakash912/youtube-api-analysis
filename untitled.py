6049c3b67654ff10694bae9cf2ed4590c08768a1
Venkata Guru Sai Akash Kollati <venkatagurusaiakashkollati@Venkatas-MacBook-Air.local>
2023-10-18 00:48:05 -0400

    Added files

---
+++ untitled.py
@@ -0,0 +1,22 @@
+def get_channel_stats(youtube,channel_ids):
+
+    all_data = []    
+
+    request = youtube.channels().list(
+        part = "snippet,contentDetails,statistics",
+        id=','.join(channel_ids)
+    )
+    response = request.execute()
+
+    # loop through items
+    for item in response['items']:
+        data = {'channelName': item['snippet']['title'],
+                'subscribers': item['statistics']['subscriberCount'],
+                'views': item['statistics']['viewCount'],
+                'totalVideos': item['statistics']['videoCount'],
+                'playlistId': item['contentDetails']['relatedPlaylists']['uploads']
+        }
+        
+        all_data.append(data)
+        
+    return pd.DataFrame(all_data)
